51f1df5ad7e0a963254d4a5479456ba46fbb3ee2
Tim Oliver <me@timoliver.com.au>
2018-12-25 00:33:14 +0800

    Added keychain access file

---
+++ TOFileKit/Models/Services/TOFileKeychainAccess.h
@@ -0,0 +1,17 @@
+//
+//  TOFileKeychainAccess.h
+//  TOFileKitExample
+//
+//  Created by Tim Oliver on 25/12/18.
+//  Copyright © 2018 Tim Oliver. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TOFileKeychainAccess : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TOFileKit/Models/Services/TOFileKeychainAccess.m
@@ -0,0 +1,13 @@
+//
+//  TOFileKeychainAccess.m
+//  TOFileKitExample
+//
+//  Created by Tim Oliver on 25/12/18.
+//  Copyright © 2018 Tim Oliver. All rights reserved.
+//
+
+#import "TOFileKeychainAccess.h"
+
+@implementation TOFileKeychainAccess
+
+@end
