52c8a00e8f109aa475e0415ffb3a3b9076809db6
Tim Oliver <me@timoliver.com.au>
2018-12-25 11:59:03 +0800

    Added back interface definition

--- TOFileKitTests/TOFileKeychainAccessTests.m
+++ TOFileKitTests/TOFileKeychainAccessTests.m
@@ -19,11 +19,13 @@
 //  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
 //  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
 //  IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
 #import <XCTest/XCTest.h>
 #import "TOFileKeychainAccess.h"
 
 NSString * const kTOFileKeychainIdentifier = @"net.timoliver.tofilekit.test";
+
+@interface TOFileKeychainAccessTests : XCTestCase
+@end
 
 @implementation TOFileKeychainAccessTests
 
