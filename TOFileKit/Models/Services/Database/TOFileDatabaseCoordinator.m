6a0d54dbf6b2c1e11adf6645ae58cc2cff9651a6
Tim Oliver <me@timoliver.com.au>
2019-02-01 01:12:00 +0900

    Added Realm Configuration creation logic

--- TOFileKit/Models/Services/Database/TOFileDatabaseCoordinator.m
+++ TOFileKit/Models/Services/Database/TOFileDatabaseCoordinator.m
@@ -31,9 +31,15 @@
 
 @property (nonatomic, strong) TOFileKeychainAccess *keychainAccess;
 
+@property (nonatomic, strong, readwrite) RLMRealmConfiguration *realmConfiguration;
+
+@property (nonatomic, readonly) NSData *encryptionKey;
+
 @end
 
 @implementation TOFileDatabaseCoordinator
+
+#pragma mark - Class Creation -
 
 - (instancetype)initWithFileURL:(NSURL *)fileURL keychainIdentifier:(nullable NSString *)keychainIdentifier;
 {
@@ -49,6 +55,75 @@
     
     return self;
 }
+
+#pragma mark - Realm Configuration Accessor
+
+- (RLMRealmConfiguration *)realmConfiguration
+{
+    // Once we've successfully created a Realm configuration, re-use it after that
+    if (_realmConfiguration) { return _realmConfiguration; }
+
+    // Create a new Realm configuration
+    _realmConfiguration = [self newRealmConfiguration];
+
+    // Try and create a Realm instance with it to see if any errors occur.
+    NSError *error = nil;
+    @autoreleasepool {
+        [RLMRealm realmWithConfiguration:_realmConfiguration error:&error];
+    }
+
+    // If an error occurred, the file is either corrupt, or the encryption key was wrong.
+    // Both of those scenarios are irrecoverable. Delete the file.
+    if (error) {
+        [[NSFileManager defaultManager] removeItemAtURL:self.fileURL error:nil];
+    }
+
+    // The file will be recreated next time the configuration is called, so we can just return it.
+    return _realmConfiguration;
+}
+
+#pragma mark - Configuration Sanity Checker Methods -
+
+- (NSData *)encryptionKey
+{
+    if (self.keychainAccess == nil) { return nil; }
+
+    NSError *error = nil;
+    NSData *encryptionKey = [self.keychainAccess dataFromKeychainWithError:&error];
+
+    // Return if we got an object back and there were no errors
+    if (encryptionKey && !error) { return encryptionKey; }
+
+    NSException *exception = [NSException exceptionWithName:NSInternalInconsistencyException reason:@"Unable to edit security keychain entry." userInfo:nil];
+
+    // If we were unable to get at a pre-made key because of some error, delete the key so we can regenerate it.
+    // If the delete operation fails, something is clearly wrong with this device. Instead of potentially leaking unencrypted data, throw an exception.
+    [self.keychainAccess deleteDataFromKeychainWithError:&error];
+    if (error) { [exception raise]; }
+
+    // Try to regenerate the key again. If it fails again, throw the same exception
+    encryptionKey = [self.keychainAccess dataFromKeychainWithError:&error];
+    if (error) { [exception raise]; }
+
+    return encryptionKey;
+}
+
+- (RLMRealmConfiguration *)newRealmConfiguration
+{
+    RLMRealmConfiguration *config = [[RLMRealmConfiguration alloc] init];
+    config.fileURL = self.fileURL;
+    config.encryptionKey = self.encryptionKey;
+    config.objectClasses = @[[TOFileAccount class], [TOFileAccount class], [TOFileDownload class]];
+    return config;
+}
+
+- (void)validateRealmOnDisk
+{
+    // No need to validate if the file hasn't been created yet.
+    if (!self.fileExists) { return; }
+}
+
+#pragma mark - Accessor Methods -
 
 - (BOOL)fileExists
 {
