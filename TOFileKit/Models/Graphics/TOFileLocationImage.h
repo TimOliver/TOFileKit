e7408b173367d81abd6c83da0fd4b910a4cfa748
Tim Oliver <me@timoliver.com.au>
2019-02-16 13:49:38 +0900

    Added class for generating icon assets

---
+++ TOFileKit/Models/Graphics/TOFileLocationImage.h
@@ -0,0 +1,17 @@
+//
+//  TOFileLocationImage.h
+//  TOFileKitExample
+//
+//  Created by Tim Oliver on 16/2/19.
+//  Copyright © 2019 Tim Oliver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TOFileLocationImage : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TOFileKit/Models/Graphics/TOFileLocationImage.m
@@ -0,0 +1,21 @@
+//
+//  TOFileLocationImage.m
+//  TOFileKitExample
+//
+//  Created by Tim Oliver on 16/2/19.
+//  Copyright © 2019 Tim Oliver. All rights reserved.
+//
+
+#import "TOFileLocationImage.h"
+
+@implementation TOFileLocationImage
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
