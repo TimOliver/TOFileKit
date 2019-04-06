b634b298faa80797ec078069a61180816fb06e62
Tim Oliver <me@timoliver.com.au>
2019-04-06 22:42:50 +0900

    Ensured test is running

--- TOFileKitTests/Models/Services/TOFileLocalServiceDiscoveryTests.m
+++ TOFileKitTests/Models/Services/TOFileLocalServiceDiscoveryTests.m
@@ -15,9 +15,25 @@
 
 @implementation TOFileLocalServiceDiscoveryTests
 
+// Do a basic test to see that the class will properly handle services sent from Bonjour
 - (void)testLocalDiscovery
 {
+    NSArray *serviceTypes = @[@"_smb._tcp."];
 
+    // Allocation
+    TOFileLocalServiceDiscovery *discovery = [[TOFileLocalServiceDiscovery alloc] initWithSearchServiceTypes:serviceTypes];
+    XCTAssertNotNil(discovery);
+
+    discovery.serviceListUpdatedHandler = ^{
+        XCTAssertTrue(discovery.services.count > 0);
+    };
+
+    // Create a dummy service
+    NSNetService *service = [[NSNetService alloc] initWithDomain:@"" type:@"_smb._tcp." name:@"Test"];
+
+    // Expose the protocol conformance of the object so we can trigger a fake service detection
+    id<NSNetServiceBrowserDelegate> discoveryDelegate = (id)discovery;
+    [discoveryDelegate netServiceBrowser:[[NSNetServiceBrowser alloc] init] didFindService:service moreComing:YES];
 }
 
 @end
