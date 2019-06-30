1ae8b78c9e652eb323ebd11d37b828a157b95faf
Tim Oliver <me@timoliver.com.au>
2019-07-01 00:06:28 +0900

    Added header implementation for picker presentation

--- TOFileKit/Presenters/FileLocationPicker/TOFileLocationPickerPresenter.h
+++ TOFileKit/Presenters/FileLocationPicker/TOFileLocationPickerPresenter.h
@@ -7,10 +7,28 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "TOFileConstants.h"
+
+@class TOFileCoordinator;
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface TOFileLocationPickerPresenter : NSObject
+
+/* Number of sections */
+- (NSInteger)numberOfSections;
+
+/* Number of items in each section */
+- (NSInteger)numberOfItemsInSection:(NSInteger)section;
+
+/* The name of the item at the specific location */
+- (NSString *)nameOfItemInSection:(NSInteger)section atIndex:(NSInteger)index;
+
+/** The service type of the location for the given item */
+- (TOFileServiceType)typeOfItemInIndex:(NSInteger)index section:(NSInteger)section;
+
+/** Create a new instance with the provided file coordinator */
+- (instancetype)initWithFileCoordinator:(TOFileCoordinator *)fileCoordinator;
 
 @end
 
