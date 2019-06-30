39943a6aa8452392cc816b9fd9ec0edfc6d14582
Tim Oliver <me@timoliver.com.au>
2019-06-30 13:28:17 +0900

    Implemented centered table title view

--- TOFileKit/Views/Common/TableView/Accessories/TOFileTableTitleHeaderView.h
+++ TOFileKit/Views/Common/TableView/Accessories/TOFileTableTitleHeaderView.h
@@ -10,7 +10,26 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+/**
+ A table view header view that displays a center-aligned
+ title and message above all of the table rows
+ */
 @interface TOFileTableTitleHeaderView : UIView
+
+/** The title text displayed in large sizing alongt the top */
+@property (nonatomic, copy) NSString *title;
+
+/** The message text displayed in standard sizing underneath the title */
+@property (nonatomic, copy) NSString *message;
+
+/** The color of the title text */
+@property (nonatomic, strong) UIColor *titleColor;
+
+/** The color of the message text */
+@property (nonatomic, strong) UIColor *messageColor;
+
+/** Create a new instance of the header view with the provided title and message */
+- (instancetype)initWithTitle:(NSString *)title message:(NSString *)message;
 
 @end
 
--- TOFileKit/Views/Common/TableView/Accessories/TOFileTableTitleHeaderView.m
+++ TOFileKit/Views/Common/TableView/Accessories/TOFileTableTitleHeaderView.m
@@ -8,14 +8,132 @@
 
 #import "TOFileTableTitleHeaderView.h"
 
+@interface TOFileTableTitleHeaderView ()
+
+@property (nonatomic, strong) UILabel *titleLabel;
+@property (nonatomic, strong) UILabel *messageLabel;
+
+@property (nonatomic, assign) CGFloat textSpacing;
+
+@end
+
 @implementation TOFileTableTitleHeaderView
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+#pragma mark - View Creation
+
+- (instancetype)initWithTitle:(NSString *)title message:(NSString *)message
+{
+    if (self = [super initWithFrame:(CGRect){0,0,320,60}]) {
+        _title = title;
+        _message = message;
+        [self commonInit];
+    }
+
+    return self;
 }
-*/
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    if (self = [super initWithFrame:frame]) {
+        [self commonInit];
+    }
+
+    return self;
+}
+
+- (void)commonInit
+{
+    _textSpacing = 5.0f;
+    self.backgroundColor = [UIColor whiteColor];
+
+    // Create a dynamic font for the title
+    UIFont *titleFont = [UIFont systemFontOfSize:28.0f weight:UIFontWeightBold];
+    if (@available(iOS 11.0, *)) {
+        UIFontMetrics *metrics = [[UIFontMetrics alloc] initForTextStyle:UIFontTextStyleTitle1];
+        titleFont = [metrics scaledFontForFont:titleFont];
+    }
+
+    // Create the title label
+    self.titleLabel = [[UILabel alloc] initWithFrame:CGRectZero];
+    self.titleLabel.font = titleFont;
+    self.titleLabel.textAlignment = NSTextAlignmentCenter;
+    self.titleLabel.textColor = [UIColor blackColor];
+    self.titleLabel.text = self.title;
+    self.titleLabel.backgroundColor = self.backgroundColor;
+    [self addSubview:self.titleLabel];
+
+    // Create the message label
+    self.messageLabel = [[UILabel alloc] initWithFrame:CGRectZero];
+    self.messageLabel.font = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
+    self.messageLabel.textAlignment = NSTextAlignmentCenter;
+    self.messageLabel.backgroundColor = self.backgroundColor;
+    self.messageLabel.textColor = [UIColor blackColor];
+    self.messageLabel.text = self.message;
+    [self addSubview:self.messageLabel];
+}
+
+#pragma mark - Content Layout -
+
+- (void)layoutSubviews
+{
+    [super layoutSubviews];
+
+    // View constants
+    CGFloat midX = CGRectGetMidX(self.bounds);
+    CGFloat width = CGRectGetWidth(self.bounds);
+
+    // Work out the available size we can afford for the content
+    UIEdgeInsets margins = self.layoutMargins;
+    CGSize contentSize = (CGSize){width - (margins.left + margins.right), CGFLOAT_MAX};
+    CGRect contentBounds = (CGRect){CGPointZero, contentSize};
+
+    // Update y to include the height of previous views
+    CGFloat y = margins.top;
+
+    // Lay out the title label
+    CGRect frame = self.titleLabel.frame;
+    frame.size = [self.titleLabel textRectForBounds:contentBounds limitedToNumberOfLines:0].size;
+    frame.origin.x = midX - (frame.size.width * 0.5f);
+    frame.origin.y = y;
+    self.titleLabel.frame = CGRectIntegral(frame);
+
+    y += CGRectGetMaxY(frame) + self.textSpacing;
+
+    // Lay out the message label
+    frame = self.messageLabel.frame;
+    frame.size = [self.messageLabel textRectForBounds:contentBounds limitedToNumberOfLines:0].size;
+    frame.origin.x = midX - (frame.size.width * 0.5f);
+    frame.origin.y = y;
+    self.messageLabel.frame = CGRectIntegral(frame);
+}
+
+- (void)sizeToFitInWidth:(CGFloat)width
+{
+    // Work out the available size we can afford for the content
+    UIEdgeInsets margins = self.layoutMargins;
+    CGSize contentSize = (CGSize){width - (margins.left + margins.right), CGFLOAT_MAX};
+    CGRect contentBounds = (CGRect){CGPointZero, contentSize};
+
+    // Set up the new frame
+    CGRect frame = self.frame;
+    frame.size.width = width;
+
+    // Start with the outer level margins
+    frame.size.height = margins.top + margins.bottom;
+
+    // Add the height of the title
+    frame.size.height += [self.titleLabel textRectForBounds:contentBounds
+                                     limitedToNumberOfLines:0].size.height;
+
+    // Add the spacing between the two labels
+    frame.size.height += self.textSpacing;
+
+    // Add the message label size
+    frame.size.height += [self.messageLabel textRectForBounds:contentBounds
+                                       limitedToNumberOfLines:0].size.height;
+
+    // Resize ourselves back to that size
+    self.frame = frame;
+}
 
 @end
