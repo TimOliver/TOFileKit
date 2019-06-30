dfc113cccf724a3b9b1fc34b20c7928ac88b5096
Tim Oliver <me@timoliver.com.au>
2019-06-30 23:36:31 +0900

    Added presenter object

---
+++ TOFileKit/Presenters/FileLocationPicker/TOFileLocationPickerPresenter.h
@@ -0,0 +1,17 @@
+//
+//  TOFileLocationPickerPresenter.h
+//  TOFileKitExample
+//
+//  Created by Tim Oliver on 30/6/19.
+//  Copyright © 2019 Tim Oliver. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TOFileLocationPickerPresenter : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TOFileKit/Presenters/FileLocationPicker/TOFileLocationPickerPresenter.m
@@ -0,0 +1,13 @@
+//
+//  TOFileLocationPickerPresenter.m
+//  TOFileKitExample
+//
+//  Created by Tim Oliver on 30/6/19.
+//  Copyright © 2019 Tim Oliver. All rights reserved.
+//
+
+#import "TOFileLocationPickerPresenter.h"
+
+@implementation TOFileLocationPickerPresenter
+
+@end
