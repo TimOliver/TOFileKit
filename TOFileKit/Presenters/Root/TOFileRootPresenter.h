ae5151d9ce15266503b9186b9d28629a611a1ec3
Tim Oliver <me@timoliver.com.au>
2019-07-27 12:35:31 +0530

    Added root presenter

---
+++ TOFileKit/Presenters/Root/TOFileRootPresenter.h
@@ -0,0 +1,17 @@
+//
+//  TOFileRootPresenter.h
+//  TOFileKitExample
+//
+//  Created by Tim Oliver on 27/7/19.
+//  Copyright © 2019 Tim Oliver. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TOFileRootPresenter : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
