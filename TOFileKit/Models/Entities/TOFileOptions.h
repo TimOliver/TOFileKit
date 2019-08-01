0c680f9322437799e940bcd98075911a3dba2230
Tim Oliver <me@timoliver.com.au>
2019-08-02 01:43:22 +0900

    Added options persistence entity

---
+++ TOFileKit/Models/Entities/TOFileOptions.h
@@ -0,0 +1,17 @@
+//
+//  TOFileOptions.h
+//  TOFileKitExample
+//
+//  Created by Tim Oliver on 2/8/19.
+//  Copyright © 2019 Tim Oliver. All rights reserved.
+//
+
+#import "RLMObject.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TOFileOptions : RLMObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TOFileKit/Models/Entities/TOFileOptions.m
@@ -0,0 +1,13 @@
+//
+//  TOFileOptions.m
+//  TOFileKitExample
+//
+//  Created by Tim Oliver on 2/8/19.
+//  Copyright © 2019 Tim Oliver. All rights reserved.
+//
+
+#import "TOFileOptions.h"
+
+@implementation TOFileOptions
+
+@end
